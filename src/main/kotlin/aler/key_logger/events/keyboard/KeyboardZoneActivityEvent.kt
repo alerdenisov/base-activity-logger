4e51fc8821070e492327fe0e81f4dfca7944570d
Aler Denisov <aler.zampillo@gmail.com>
2017-08-15 00:37:44 +0300

    Add MIT license comments

--- src/main/kotlin/aler/key_logger/events/keyboard/KeyboardZoneActivityEvent.kt
+++ src/main/kotlin/aler/key_logger/events/keyboard/KeyboardZoneActivityEvent.kt
@@ -1,3 +1,23 @@
+// Copyright (c) 2017 Aler Denisov <aler.zampillo@gmail.com>
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 package aler.key_logger.events.keyboard
 
 import aler.key_logger.events.EventType
@@ -9,13 +29,6 @@ class KeyboardZoneActivityEvent() : LoggerEvent, LoggerData {
     val counts = Array(KeyboardZoneType.values().size, { i -> 0 })
 
     override fun toJson(): String = """{ "event": "${getType()}", "code": "activity", "data": [ ${counts.joinToString()} ] }"""
-//    override fun toHex(): String = (toStringArray(//(toByteArray(
-//            getType(),
-//            KeyboardEventType.Press,
-//            keyZone,
-//            count
-//    ))
-
     override fun getType(): EventType = EventType.Keyboard
     override fun getData(): LoggerData = this
 }
--- src/main/kotlin/aler/key_logger/events/mouse/MouseClickEvent.kt
+++ src/main/kotlin/aler/key_logger/events/mouse/MouseClickEvent.kt
@@ -1,3 +1,23 @@
+// Copyright (c) 2017 Aler Denisov <aler.zampillo@gmail.com>
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 package aler.key_logger.events.mouse
 
 import aler.key_logger.events.EventType
@@ -6,12 +26,6 @@ import aler.key_logger.events.LoggerEvent
 
 class MouseClickEvent(var count : Int) : LoggerEvent, LoggerData {
     override fun toJson(): String = """{ "event": "${getType()}", "code": "clicks", "data": ${count} }"""
-    //    override fun toHex(): String = (toStringArray(//(toByteArray(
-//            getType(),
-//            MouseEventType.Click,
-//            x,
-//            y
-//    ))
     override fun getType(): EventType = EventType.Mouse
     override fun getData(): LoggerData = this
 }
